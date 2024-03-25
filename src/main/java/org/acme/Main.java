cd333610fa945831f1e32c4d2277d004f81955a4
leo-d-a <damerval.leo@proton.me>
2024-03-25 12:00:16 +0100

    debug: build, quarkusDev and Run successful, Main app working

--- src/main/java/org/acme/Main.java
+++ src/main/java/org/acme/Main.java
@@ -3,7 +3,5 @@ package org.acme;
 import io.quarkus.runtime.Quarkus;
 
 public class Main {
-        public static void main(String[] args) {
-            Quarkus.run(args);
-        }
+        public static void main(String[] args) {Quarkus.run(args);}
 }
